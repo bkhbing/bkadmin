ffab5a80379293f0886d02beda699ff31657f011
bkhbing <1787626070@qq.com>
2023-05-24 12:57:47 +0800

    添加spring boot项目

---
+++ src/main/java/com/bkhb/bkadmin/BkadminApplication.java
@@ -0,0 +1,13 @@
+package com.bkhb.bkadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BkadminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bkhb/bkadmin/BkadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkhb.bkadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
